63122712b8ecbf3370ca2a91a939894783d6a9c1
Leonardo César <leocesar1977@Gmail.com>
2025-11-29 17:32:19 -0300

    Exer49

--- curso_java_basico/src/parte_3/Exer49.java
+++ curso_java_basico/src/parte_3/Exer49.java
@@ -3,8 +3,20 @@ package parte_3;
 public class Exer49 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Compare o uso de operador ternário com if-else:
+		//quando é mais apropriado usar cada um?
+		System.out.println("O operador ternário é uma forma concisa de  ");
+		System.out.println("expressar uma condicional simples que retorna um valor.");
+		System.out.println("Como é uma atribuição condicional simples, é ideal");
+		System.out.println("para atribuir um de dois valores possíveis a uma ");
+		System.out.println("variável com base em uma única condição.");
+		System.out.println("\n\n");
+		System.out.println("A estrutura if-else é um bloco de controle");
+		System.out.println("de fluxo mais versátil e flexível.");
+		System.out.println("É uma lógica complexa de Múltiplas Linhas, quando a ");
+		System.out.println("lógica dentro do bloco condicional envolve várias ");
+		System.out.println("operações, ou múltiplas instruções,");
+		System.out.println("o if-else é essencial para manter a clareza do código.");
 	}
 
 }
