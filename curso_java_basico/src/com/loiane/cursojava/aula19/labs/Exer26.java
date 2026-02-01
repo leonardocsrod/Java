1c1093d3ca7d28c0ab97a5db6a4d6bebd0106517
Leonardo César <leocesar1977@Gmail.com>
2026-02-01 20:03:43 -0300

    exerLoiane

--- curso_java_basico/src/com/loiane/cursojava/aula19/labs/Exer26.java
+++ curso_java_basico/src/com/loiane/cursojava/aula19/labs/Exer26.java
@@ -2,7 +2,7 @@ package com.loiane.cursojava.aula19.labs;
 
 import java.util.Scanner;
 
-public class Exer026 {
+public class Exer26 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
